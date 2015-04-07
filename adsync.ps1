8544fff0bfd4f62d4b5e1457784260405cd16bfa
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2015-04-08 00:12:15 +0200

    Create adsync.ps1

---
+++ adsync.ps1
@@ -0,0 +1,29 @@
+# http://openclassrooms.com/forum/sujet/powershell-stocker-get-aduser-dans-une-variable
+
+$Zimbra_Hostname = "zimbra"
+
+$username = $env:username.ToLower()
+$domain = $env:userdnsdomain.ToLower()
+
+$Accounts_file = "\\$Zimbra_Hostname.$domain\adsync\Accounts.txt"
+$Mails_file = "\\$Zimbra_Hostname.$domain\adsync\Mails.txt"
+
+$GetADUser = Get-ADUser -Filter 'SamAccountName -eq "$env:username"' -Property *
+ 
+foreach($donnees in $GetADUser)
+{
+	$Surname = $donnees.Surname
+	$GivenName = $donnees.GivenName
+	$LogonCount = $donnees.LogonCount
+	$Accounts = "$Surname" + ":" + "$GivenName" + ":" + "$username@$domain"
+	$Mails = "$username@$domain"
+}
+
+if($LogonCount -lt 2)
+{
+	ADD-content -path $Accounts_file -value "$Accounts"
+	ADD-content -path $Mails_file -value "$Mails"
+}
+
+Write-Host "Appuyez sur une touche pour continuer..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
