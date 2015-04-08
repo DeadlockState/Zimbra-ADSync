f0a8cd6a96194b3e109888341448e56b7363c6d7
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2015-04-08 02:17:19 +0200

    Create adsync.ps1

---
+++ adsync.ps1
@@ -0,0 +1,31 @@
+# Steven CHARRIER - http://stevencharrier.fr/
+# https://github.com/Punk--Rock/Zimbra-ADSync
+# Version 1.0 - 07/04/2015
+
+$Zimbra_Hostname = "zimbra"
+
+$username = $env:username.ToLower()
+$domain = $env:userdnsdomain.ToLower()
+
+$Accounts_file = "\\$Zimbra_Hostname.$domain\adsync\Accounts.txt"
+$Mails_file = "\\$Zimbra_Hostname.$domain\adsync\Mails.txt"
+
+$GetADUser = Get-ADUser -Filter 'SamAccountName -eq "$env:username"' -Property *
+ 
+foreach($donnees in $GetADUser)
+{
+	$Surname = $donnees.Surname
+	$GivenName = $donnees.GivenName
+	$LogonCount = $donnees.LogonCount
+	$Accounts = "$Surname" + ":" + "$GivenName" + ":" + "$username@$domain"
+	$Mails = "$username@$domain"
+}
+
+if($LogonCount -lt 2)
+{
+	ADD-content -path $Accounts_file -value "$Accounts"
+	ADD-content -path $Mails_file -value "$Mails"
+}
+
+Write-Host "Appuyez sur une touche pour continuer..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
